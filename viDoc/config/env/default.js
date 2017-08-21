4bd0cad66d31bb39aad4dc1e24e4096dc57a2a46
D-Mino <tranxuanquoc96@gmail.com>
2017-08-21 16:32:51 +0700

    change name

--- viDoc/config/env/default.js
+++ viDoc/config/env/default.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   app: {
-    title: 'CyberAir',
+    title: 'ViDoc',
     description: 'The Example of Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
