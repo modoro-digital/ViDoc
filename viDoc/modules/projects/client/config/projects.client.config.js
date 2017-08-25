3e618bf63a90f8802495a5e760afcc00ff23edb4
ishieric <ngocnam071094@gmail.com>
2017-08-25 16:13:07 +0700

    #3  Edit memu project left

--- viDoc/modules/projects/client/config/projects.client.config.js
+++ viDoc/modules/projects/client/config/projects.client.config.js
@@ -12,21 +12,7 @@
     menuService.addMenuItem('sidebar', {
       title: 'Projects',
       state: 'projects',
-      type: 'dropdown',
       roles: ['*']
-    });
-
-    // Add the dropdown list item
-    menuService.addSubMenuItem('sidebar', 'projects', {
-      title: 'List Projects',
-      state: 'projects.list'
-    });
-
-    // Add the dropdown create item
-    menuService.addSubMenuItem('sidebar', 'projects', {
-      title: 'Create Project',
-      state: 'projects.create',
-      roles: ['user']
     });
   }
 }());
